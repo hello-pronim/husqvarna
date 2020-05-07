8627936c564b267c7ae5a0995c14b4b160436d9b
ezhov43 <yuriyes43@gmail.com>
2020-05-07 13:22:28 +0900

    fix login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+
 
 class LoginController extends Controller
 {
@@ -37,4 +40,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function validateLogin(Request $request)
+    {
+        $request->validate([
+            'メールアドレス' => 'required|email|string',
+            'パスワード' => 'required|string',
+        ]);
+    }
+  
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-lg-8 col-md-12">
             <div class="card pt-80">
                 <div class="card-header">                    
                     <img src="{{asset('images/logo.png')}}">
@@ -11,30 +11,28 @@
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate>
                         @csrf
 
                         <div class="form-group row">                            
-                            <div class="col-md-4 offset-md-4">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control form-rounded @error('メールアドレス') is-invalid @enderror" name="メールアドレス" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{ __('メールアドレス') }}">                                
+                                @error('メールアドレス')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
+                                @enderror                                
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-4 offset-md-4">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password" class="form-control form-rounded @error('パスワード') is-invalid @enderror" name="パスワード" required autocomplete="current-password" placeholder="{{ __('パスワード') }}">
+                                @error('パスワード')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
+                                @enderror                                
                             </div>
                         </div>
 
@@ -51,7 +49,7 @@
                         </div> -->
 
                         <div class="form-group row mb-0">                            
-                            <div class="col-md-4 offset-md-4">                                
+                            <div class="col-md-6 offset-md-3">                                
                                 <button type="submit" class="btn btn-round btn-danger">
                                     {{ __('ログイン') }}
                                 </button>
