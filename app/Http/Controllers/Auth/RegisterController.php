dbb60977110b2e69ce14a367cca6732bbdea505c
pavelk43 <yuriyes43@gmail.com>
2020-06-06 19:26:00 +0900

    no login after add user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,7 +79,7 @@ class RegisterController extends Controller
        
         event(new Registered($user = $this->create($request->all())));
 
-        $this->guard()->login($user);
+        //$this->guard()->login($user);
 
         if ($response = $this->registered($request, $user)) {
             return $response;
