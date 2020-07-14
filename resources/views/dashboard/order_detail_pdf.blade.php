26fc69d6e9d28651ba9f131f4014238cdf270ace
Andrei Iunda <peterpan120@yandex.com>
2020-07-15 04:46:38 +1000

    add fonts for japanese characters in pdf

--- resources/views/dashboard/order_detail_pdf.blade.php
+++ resources/views/dashboard/order_detail_pdf.blade.php
@@ -8,33 +8,71 @@
     <title></title>
     <style type="text/css">
       table#table-order-detail{
-        border:1px solid #CCC;
         width: 100%;
         padding:0px;
         font-size:16px;
         table-collapse:collapse !important;
         page-break-inside: avoid;
       }
-      table#table-order-detail td{
-        border:1px solid #CCC;
+      table#table-order-detail thead td{
+        text-align:center;
+        border:3px solid #CCC; 
+      }
+      table#table-order-detail tbody td{
+        border:2px solid #CCC;
         padding: 10px;
         height: 45px;
       }
-      table#table-order-detail th{
-        text-align:center;
-        border:2px solid #CCC; 
-      }
       @font-face {
-        font-family: \'mgenplus\';
+        font-family: mspgothic;
         font-style: normal;
         font-weight: 400;
-        src: url(dompdf/fonts/MPLUSRounded1c-Regular.ttf);
+        src: url('{{public_path('fonts/mspgothic.ttf')}}');
       }
-      *{ font-family: mgenplus !important;}
-      @page { margin: 50px 50px; }
+      @font-face {
+        font-family: kochi-gothic;
+        font-style: normal;
+        font-weight: 400;
+        src: url('{{public_path('fonts/kochi-gothic-subst.ttf')}}');
+      }
+      @font-face {
+        font-family: kochi-mincho;
+        font-style: normal;
+        font-weight: 400;
+        src: url('{{public_path('fonts/kochi-mincho-subst-original.ttf')}}');
+      }
+      @font-face {
+        font-family: tahoma;
+        font-style: normal;
+        font-weight: 400;
+        src: url('{{public_path('fonts/Tahoma Regular font.ttf')}}');
+      }
+      *{ 
+        font-family: mspgothic, kochi-gothic, kochi-mincho, tahoma !important;
+        color: #3E3E3E;
+      }
+      @page { margin: 60px 30px; }
       #pdf-header { position: fixed; left: 0px; top: -50px; right: 0px; height: 50px;}
       .text-bold{
         font-weight: bold!important;
+      }
+      .text-black{
+        color: black!important;
+      }
+      .fs-24{
+        font-size: 24px!important;
+      }
+      .fs-20{
+        font-size: 20px!important;
+      }
+      .fs-16{
+        font-size: 16px!important;
+      }
+      .fs-14{
+        font-size: 14px!important;
+      }
+      .mr-20{
+        margin-right: 20px!important;
       }
       .pdf-header-item{display: inline-block;}
     </style>
@@ -42,13 +80,13 @@
   <body>
     <div id="pdf-header">
       <table style="width: 100%">
-        <tr>
+        <tr class="text-black fs-20">
           <td>
-            <h3>Amazon <small>0715000</small></h3>
+            <span class="fs-24 text-bold mr-20">Amazon</span><span>0715000</span>
           </td>
           <td style="text-align: center;">
             <p>
-              <span>配送先:</span><span style="margin-right: 20px">{{explode(" - ", $orders[0]->ship_location)[0]}}</span>
+              <span>配送先:</span><span class="mr-20">{{explode(" - ", $orders[0]->ship_location)[0]}}</span>
               <span>コード:</span><span>0715025</span>
             </p>
           </td>
@@ -64,19 +102,19 @@
       <table class="table table-bordered" id="table-order-detail">
         <thead>
           <tr>
-            <th></th>
-            <th>PO</th>
-            <th>配送センター</th>
-            <th>ASIN</th>
-            <th>商品名</th>
-            <th>数量</th>
-            <th>価格</th>
+            <td></td>
+            <td>PO</td>
+            <td>配送センター</td>
+            <td>ASIN</td>
+            <td>商品名</td>
+            <td>数量</td>
+            <td>価格</td>
           </tr>
         </thead>
         <tbody>
         @for($j=0; $j<$limit_per_page; $j++)
           @if(isset($orders[$i*$limit_per_page+$j]))
-          <tr>
+          <tr class="fs-16">
             <td align="center">{{$i*$limit_per_page+$j+1}}</td>
             <td align="center" class="text-bold">{{$orders[$i*$limit_per_page+$j]->po}}</td>
             <td align="left">{{$orders[$i*$limit_per_page+$j]->ship_location}}</td>
